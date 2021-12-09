b44ed030bdc6579e3e5d6fe9e31b78a065fbcbd6
bzeier <43791891+bzeier@users.noreply.github.com>
2021-12-09 19:58:20 +0100

    Initial commit

---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    internal class ToDoItem
+    {
+        public string title = "";
+        public string description = "";
+
+        public ToDoItem(string _title, string _desc)
+        {
+            title = _title;
+            description = _desc;
+        }
+
+    }
+}
---
+++ ToDoApp/Services/ToDoManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services
+{
+    internal class ToDoManager
+    {
+        public List<ToDoItem> ToDoList = new List<ToDoItem>();
+
+        public void AddToDoItem(string title, string description)
+        {
+            var newItem = new ToDoItem(title, description);
+            ToDoList.Add(newItem);
+        }
+    }
+}
